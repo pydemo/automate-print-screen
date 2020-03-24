b19965c4d234eaaa304c40c4de92977241d1eb3f
pydemo <36740585+pydemo@users.noreply.github.com>
2020-03-24 12:51:59 -0400

    Save screen shot

--- ps.py
+++ ps.py
@@ -1,5 +1,8 @@
 import sys
 from pprint import pprint as pp
+from pynput.keyboard import Key, Listener, Controller
+import click
+click.disable_unicode_literals_warning = True
 
 def print_screen_png(fname):
 	import numpy as np
@@ -8,29 +11,34 @@ def print_screen_png(fname):
 	image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 	cv2.imwrite(fname, image)
 
-if __name__ == "__main__":
-		
+@click.command()
+@click.option('-f', 	'--file_name', 			default = 'snap', 	help = 'File prefix.', required=True )
 
-	pid=0
-	from pynput.keyboard import Key, Listener, Controller
-	KB = Controller()
+def start_loop(**kwargs):
+	
+	fn=kwargs.get('file_name')
+	assert fn
+	
 	def on_press(key):
 		global pid
 		if key == Key.print_screen:
-			print(ctrlon, '{0} pressed'.format(key))
-			print_screen_png('test_%03d.png' % pid)
+			print('{0} pressed'.format(key))
+			pngfn='%s_%03d.png' % (fn,pid)
+			print_screen_png(pngfn)
+			print('Screenshot %d is saved to "%s"' % (pid, pngfn))
 			pid +=1
 
-
-
 	def on_release(key):
-		global ctrlon
 		if key == Key.esc:
 			# Stop listener
 			return False
-
 
 	with Listener(
 			on_press=on_press,
 			on_release=on_release) as listener:
 		listener.join()	
+		
+if __name__ == "__main__":
+	pid=0
+	start_loop()
+
