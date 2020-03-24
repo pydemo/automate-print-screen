bf73fb3b7a0754265b18329f396ae743ff69a916
pydemo <36740585+pydemo@users.noreply.github.com>
2020-03-24 12:18:32 -0400

    Creates screen snaps when you press `Print Screen'
    Runs in background and saves screens into "snap_XXX.png" file each time you pres "Print Screen" on keyboard.

---
+++ ps.py
@@ -0,0 +1,37 @@
+import sys
+from pprint import pprint as pp
+
+def print_screen_png(fname):
+	import numpy as np
+	import pyautogui,cv2
+	image = pyautogui.screenshot()
+	image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+	cv2.imwrite(fname, image)
+
+if __name__ == "__main__":
+		
+
+	pid=0
+	from pynput.keyboard import Key, Listener, Controller
+	KB = Controller()
+	def on_press(key):
+		global pid
+		if key == Key.print_screen:
+			print(ctrlon, '{0} pressed'.format(key))
+			print_screen_png('snap_%03d.png' % pid)
+			pid +=1
+
+
+
+	def on_release(key):
+		global ctrlon
+		if key == Key.esc:
+			# Stop listener
+			return False
+		if key == Key.ctrl_l:
+			ctrlon=True
+
+	with Listener(
+			on_press=on_press,
+			on_release=on_release) as listener:
+		listener.join()	
